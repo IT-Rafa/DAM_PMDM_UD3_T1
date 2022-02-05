121723c9355576bda38968527eaf79024f680f92
it-rafa <it-rafamartinez@outlook.es>
2022-02-05 02:13:31 +0100

    addin features spinnin

--- U3_STUDENT_Rafael/app/src/main/java/es/itrafa/u3_student_rafael/MainActivity.java
+++ U3_STUDENT_Rafael/app/src/main/java/es/itrafa/u3_student_rafael/MainActivity.java
@@ -5,13 +5,16 @@ import androidx.appcompat.widget.SwitchCompat;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,11 +24,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
-        EditText editText_inputText  = findViewById(R.id.editText_inputText);
+        EditText editText_inputText = findViewById(R.id.editText_inputText);
         CheckBox checkBox_clear = findViewById(R.id.checkBox_clear);
         Button button_addClear = findViewById(R.id.button_addClear);
         TextView textView_showText = findViewById(R.id.textView_showText);
+        RadioGroup radioGroup_color = findViewById(R.id.radioGroup_color);
         RadioButton radioButton_red = findViewById(R.id.radioButton_red);
         RadioButton radioButton_blue = findViewById(R.id.radioButton_blue);
         Spinner spinner_provincesList = findViewById(R.id.spinner_provincesList);
@@ -38,16 +41,44 @@ public class MainActivity extends AppCompatActivity {
 
             public void onClick(View v) {
 
-                if(checkBox_clear.isChecked()){
+                if (checkBox_clear.isChecked()) {
                     //clear
                     editText_inputText.setText((""));
-                }else{
+                    textView_showText.setText((""));
+                } else {
                     //add
                     textView_showText.append(editText_inputText.getText());
                 }
             }
         });
+
+        radioGroup_color.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.radioButton_red:
+                        textView_showText.setTextColor(getResources().getColor(R.color.red));
+                        break;
+                    case R.id.radioButton_blue:
+                        textView_showText.setTextColor(getResources().getColor(R.color.blue));
+                        break;
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + checkedId);
+                }
+            }
+        });
+
+        spinner_provincesList.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                Toast.makeText(getApplicationContext(), "Seleccionaste", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parentView) {
+                // your code here
+            }
+        });
+
     }
-
-
 }
